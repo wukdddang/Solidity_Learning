3614a30901fbdcb060fd0cb97c46f2fecf44ee0e
wukddang <dnr8874@naver.com>
2025-08-03 22:10:33 +0900

    DecentralBank 테스트 파일 업데이트: 고객 계정에 대한 Tether 및 RWD 토큰 전송 기능을 추가하고, DecentralBank 계약의 기본 이름 및 잔액 확인 테스트를 구현하여 테스트의 포괄성을 향상시켰습니다.

--- defi-staking-app-starter/test/decentral-bank.test.js
+++ defi-staking-app-starter/test/decentral-bank.test.js
@@ -6,13 +6,22 @@ require("chai")
   .use(require("chai-as-promised"))
   .should();
 
-contract("DecentralBank", (accounts) => {
+contract("DecentralBank", ([owner, customer]) => {
   let tether, rwd, decentralBank;
+
+  function tokens(number) {
+    return web3.utils.toWei(number.toString(), "ether");
+  }
 
   before(async () => {
     tether = await Tether.new();
     rwd = await RWD.new();
     decentralBank = await DecentralBank.new(rwd.address, tether.address);
+
+    await rwd.transfer(decentralBank.address, tokens("1000000"));
+    await tether.transfer(customer, tokens("100"), {
+      from: owner,
+    });
   });
 
   // 테스트 코드
@@ -27,6 +36,21 @@ contract("DecentralBank", (accounts) => {
     it("matches name successfully", async () => {
       const name = await rwd.name();
       assert.equal(name, "Reward Token");
+    });
+  });
+
+  describe("DecentralBank", async () => {
+    it("has a default name", async () => {
+      const name = await decentralBank.name();
+      assert.equal(name, "Decentral Bank");
+    });
+
+    it("contract has tokens", async () => {
+      let balance = await rwd.balanceOf(decentralBank.address);
+      assert.equal(balance.toString(), tokens("1000000"));
+
+      balance = await tether.balanceOf(decentralBank.address);
+      assert.equal(balance.toString(), tokens("0"));
     });
   });
 });
