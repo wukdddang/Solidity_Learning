c79703fb0d442ccc43dd8ce71e0ec0b585809565
wukddang <dnr8874@naver.com>
2025-08-04 00:44:18 +0900

    DecentralBank 계약 기능 추가: 스테이킹 및 언스테이킹 기능을 구현하고, 리워드 토큰 발급 기능을 추가하여 고객의 스테이킹 보상을 관리할 수 있도록 하였습니다. 관련 테스트를 추가하여 기능의 정확성을 검증하였습니다.

--- defi-staking-app-starter/src/contracts/DecentralBank.sol
+++ defi-staking-app-starter/src/contracts/DecentralBank.sol
@@ -9,8 +9,62 @@ contract DecentralBank {
   Tether public tether;
   RWD public rwd;
 
+  address[] public stakers;
+
+  mapping(address => uint) public stakingBalance;
+  mapping(address => bool) public hasStaked;
+  mapping(address => bool) public isStaking;
+
   constructor(RWD _rwd, Tether _tether) public {
     rwd = _rwd;
     tether = _tether;
+    owner = msg.sender;
+  }
+
+  function depositTokens(uint256 _amount) public {
+    require(_amount > 0, "amount cannot be 0");
+
+    // 스테이킹을 위해 테더 토큰을 컨트랙트에 입금
+    tether.transferFrom(msg.sender, address(this), _amount);
+    stakingBalance[msg.sender] += _amount;
+
+    if (!hasStaked[msg.sender]) {
+      stakers.push(msg.sender);
+    }
+
+    isStaking[msg.sender] = true;
+    hasStaked[msg.sender] = true;
+  }
+
+  function unstakeTokens() public {
+    uint256 balance = stakingBalance[msg.sender];
+    require(balance > 0, "staking balance cannot be 0");
+
+    tether.transfer(msg.sender, balance);
+    stakingBalance[msg.sender] = 0;
+
+    isStaking[msg.sender] = false;
+    hasStaked[msg.sender] = false;
+
+    for (uint i = 0; i < stakers.length; i++) {
+      if (stakers[i] == msg.sender) {
+        stakers[i] = stakers[stakers.length - 1];
+        stakers.length--;
+      }
+    }
+  }
+
+  // 리워드 토큰 발급
+  function issueTokens() public {
+    require(msg.sender == owner, "caller must be the owner");
+
+    for (uint i = 0; i < stakers.length; i++) {
+      address recipient = stakers[i];
+      uint256 balance = stakingBalance[recipient] / 9;
+
+      if (balance > 0) {
+        rwd.transfer(recipient, balance);
+      }
+    }
   }
 }
--- defi-staking-app-starter/test/decentral-bank.test.js
+++ defi-staking-app-starter/test/decentral-bank.test.js
@@ -53,4 +53,53 @@ contract("DecentralBank", ([owner, customer]) => {
       assert.equal(balance.toString(), tokens("0"));
     });
   });
+
+  describe("이자 농사", async () => {
+    it("스테이킹 보상 토큰 발급", async () => {
+      let result;
+
+      result = await tether.balanceOf(customer);
+      assert.equal(result.toString(), tokens("100"));
+
+      await tether.approve(decentralBank.address, tokens("100"), {
+        from: customer,
+      });
+      await decentralBank.depositTokens(tokens("100"), { from: customer });
+
+      // 고객의 테더 토큰 잔액 확인
+      result = await tether.balanceOf(customer);
+      assert.equal(result.toString(), tokens("0"));
+
+      // 고객이 스테이킹 되었는지 확인
+      result = await decentralBank.stakingBalance(customer);
+      assert.equal(result.toString(), tokens("100"));
+
+      // 고객이 스테이킹 되었는지 확인
+      result = await decentralBank.isStaking(customer);
+      assert.equal(result, true);
+
+      // 고객이 스테이킹 되었는지 확인
+      result = await decentralBank.hasStaked(customer);
+      assert.equal(result, true);
+
+      // 탈중앙화 은행의 테더 토큰 잔액 확인
+      result = await tether.balanceOf(decentralBank.address);
+      assert.equal(result.toString(), tokens("100"));
+
+      // 탈중앙화 은행의 리워드 토큰 잔액 확인
+      result = await rwd.balanceOf(decentralBank.address);
+      assert.equal(result.toString(), tokens("1000000"));
+
+      // 탈중앙화 은행의 리워드 토큰 발급
+      await decentralBank
+        .issueTokens({ from: customer })
+        .should.be.rejectedWith("caller must be the owner");
+
+      await decentralBank.issueTokens({ from: owner });
+
+      // 고객의 리워드 토큰 잔액 확인
+      result = await rwd.balanceOf(customer);
+      assert.equal(result.toString(), tokens("11.111111111111111111"));
+    });
+  });
 });
